599d1ef43f1719b407ed54af42f9037a09dea16f
officialgio121 <officialgio121@gmail.com>
2023-11-09 00:27:44 -0600

    Added Week 8

---
+++ Week 8 -  Flask App with DB/flask_app_with_db /my_app/catalog/routes.py
@@ -0,0 +1,63 @@
+from flask import request, jsonify, Blueprint
+from my_app import db
+from my_app.catalog.models import Product, Category
+
+catalog = Blueprint('catalog', __name__)
+
+@catalog.route('/')
+@catalog.route('/home')
+def home():
+    return "Welcome to the Catalog Home."
+
+@catalog.route('/product/<id>')
+def product(id):
+    product = Product.query.get_or_404(id)
+    return 'Product - %s, $%s' % (product.name, product.price)
+
+@catalog.route('/products')
+def products():
+    products = Product.query.all()
+    res = {}
+    for product in products:
+        res[product.id] = {
+            'name': product.name,
+            'price': product.price,
+            'category': product.category.name
+        }
+    return jsonify(res)
+
+@catalog.route('/product-create', methods=['POST',])
+def creat_product():
+    name = request.json.get('name')
+    price = request.json.get('price')
+    categ_name = request.json.get('category')
+    category = Category.query.filter_by(name=categ_name).first()
+    if not category:
+        category = Category(categ_name)
+    product = Product(name, price, category)
+    db.session.add(product)
+    db.session.commit()
+    return 'Product created.'
+
+@catalog.route('/category-create', methods=['POST', ])
+def create_category():
+    name = request.json.get('name')
+    category = Category(name)
+    db.session.add(category)
+    db.session.commit()
+    return 'Category created'
+
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    res = {}
+    for category in categories:
+        res[category.id] = {
+            'name': category.name
+        }
+        for product in category.products:
+            res[category.id]['products'] = {
+                'id': product.id,
+                'name': product.name,
+                'price': product.price}
+    return jsonify(res)
